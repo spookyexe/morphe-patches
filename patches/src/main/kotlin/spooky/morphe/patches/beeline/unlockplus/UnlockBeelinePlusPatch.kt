eb227e543d102d91b9591d6a23797b86a521a5d0
SP00KY <calm-breath-ruby@duck.com>
2026-08-05 13:35:54 +0000

    chore: update beeline patch requirements & description

--- patches/src/main/kotlin/spooky/morphe/patches/beeline/unlockplus/UnlockBeelinePlusPatch.kt
+++ patches/src/main/kotlin/spooky/morphe/patches/beeline/unlockplus/UnlockBeelinePlusPatch.kt
@@ -6,10 +6,24 @@ import app.morphe.util.returnEarly
 
 import spooky.morphe.patches.shared.Constants.COMPATIBILITY_BEELINE
 
+/**
+    This patch needs the following additional patches to function properly:
+        - Fix Firebase after re-signing
+        - Provide Original app certificate
+        - Spoof app signature
+        - Spoof install source
+
+    Credit to https://github.com/rushiranpise/morphe-patches for his universal patches.
+ */
 @Suppress("unused")
 val unlockBeelinePlusPatch = bytecodePatch(
     name = "Unlock Beeline Plus features",
-    description = "Unlocks Beeline plus features such as round-trip generation, live traffic and premium map types.",
+    description = "Unlocks Beeline plus features such as round-trip generation, live traffic and premium map types.
+    The following patches are also needed for this patch to work:
+        - Fix Firebase after re-signing (rushiranpise/morphe-patches)
+        - Provide Original app certificate (rushiranpise/morphe-patches)
+        - Spoof app signature (rushiranpise/morphe-patches)
+        - Spoof install source (rushiranpise/morphe-patches)",
     default = true
 ) {
     compatibleWith(COMPATIBILITY_BEELINE)
